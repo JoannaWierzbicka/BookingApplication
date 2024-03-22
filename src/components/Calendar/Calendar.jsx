61f38a5b2916ebd4b7c12f0c383e8f9f484f8e5c
JoannaWierzbicka <joannawierzbicka@poczta.onet.pl>
2024-03-22 14:56:14 +0100

    full calendar adjustment

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,12 +1,33 @@
 import { NavLink } from "react-router-dom";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+// import timeGridPlugin from "@fullcalendar/timegrid"
+
+const header = {
+  left: "prev,next today",
+  center: "title",
+  right: "dayGridMonth,timeGridWeek",
+};
 
 export default function Calendar() {
   return (
     <div>
       <NavLink to={"/"}>BACK</NavLink>
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridWeek" />
+      <FullCalendar
+        headerToolbar={header}
+        plugins={[dayGridPlugin]}
+        views={{
+          timeGridWeek: {
+            type: "dayGridWeek",
+            duration: { days: 7 },
+            buttonText: "week",
+          },
+      
+        }}
+        initialView="dayGridMonth"
+        editable={true}
+        droppable={true}
+      />
     </div>
   );
 }
